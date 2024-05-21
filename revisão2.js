ed3edb32a53aeab5a9a7c8ec280e388a15937120
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-05-21 10:55:56 -0300

    Create revisão2.js

---
+++ revisão2.js
@@ -0,0 +1,10 @@
+const nome = "Eduardo"
+const sobrenome = "Rodrigues"
+let nomecompleto = ""
+nomecompleto = nome+ " " +sobrenome
+
+console.log("seu nome é: ", nomecompleto)
+x = Number ("8")
+n = String (8)
+o = "8" + "5"
+console.log("valor é: ", o)
