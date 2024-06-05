9d0d457cdabd54a705464f16bb933bb1a44cbe68
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-06-05 09:03:51 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+const produtos = ["Sabão", "Shampoo", "Arroz", "Feijão", "Pão", 
+                  "Alface", "Tomate", "Pneu", "Bola", "Banana"]
+console.log(produtos);
+console.log(produtos[7]);
+console.log(produtos.length)
+produtos = []
+
+produtos.pop;
+console.log(produtos.pop())
