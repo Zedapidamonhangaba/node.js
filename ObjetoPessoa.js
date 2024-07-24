b6ba9ac2f0904af6c0cd4855beac5b6a57c7f1a1
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-07-24 08:52:00 -0300

    Create ObjetoPessoa.js

---
+++ ObjetoPessoa.js
@@ -0,0 +1,9 @@
+const ObjetoPessoa = {
+    nome:"Eduardo",
+    idade: 16,
+    cpf: "697.890.654-01"
+}
+console.log (ObjetoPessoa)
+console.log ('nome da pessoa: ${ObjetoPessoa.nome}')
+
+const lista = ["Eduardo",16,"697.890.654-01"]
