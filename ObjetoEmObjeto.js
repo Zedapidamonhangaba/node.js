c51ce5c388677e3a422328d7e3a0c5f67de7c080
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-08-05 12:16:57 -0300

    Create ObjetoEmObjeto.js

---
+++ ObjetoEmObjeto.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Robson",
+    idade: 36,
+    email: "Robso@gmail.com",
+    telefone: ["41 99523-0501", "41 99002-8969"]
+}
+
+console.log(cliente)
+
+console.log(cliente.telefone)
+
+console.log(cliente.telefone[1])
