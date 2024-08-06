2601988f7fd0b78666c180ee6e204031616d18be
Dudu <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-08-06 09:43:58 -0300

    Update ObjetoEmObjeto.js

--- ObjetoEmObjeto.js
+++ ObjetoEmObjeto.js
@@ -5,25 +5,33 @@ const cliente = {
     telefone: ["41 99523-0501", "41 99002-8969"]
 }
 
+
 console.log(cliente)
 
-console.log(cliente.telefone)
-
-console.log(cliente.telefone[1])
-
-
-
-
-
-cliente.endereco = {
+cliente.endereco = [{
     rua: "Sete de setembro",
     numero: 924,
     apartamento: true,
-    complemento:"ap 25"
-}
-
+    complemento: "ap 25"
+},
+]
 console.log(cliente)
 
-console.log(cliente.endereco)
+cliente.endereco.push(
+    {
+        rua: "24 de maio",
+        numero: 726,
+        apartamento: false,
+    }
+)
 
-console.log(cliente.endereço.rua)
+console.log("====== 3 ======")
+console.log(cliente)
+
+
+console.log("====== 4 ======")
+const listaEndAps = cliente.endereco.filter((endereco) => endereco.apartamento === true)
+
+console.log(listaEndAps)
+
+
