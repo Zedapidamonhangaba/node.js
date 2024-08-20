fadb4f90573ee8e7a98079f2acf293e3a2c99d41
Dudu malaquias <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-08-20 10:56:18 -0300

    Create livro.js

---
+++ livro.js
@@ -0,0 +1,11 @@
+const livro = {
+    nome: "1984",
+    autor: "George Orwell",
+    ano: 1949,
+    genero: "distopia",
+    preço: 29.99
+}
+
+console.log(livro.nome) //acessar um dado do objeto
+console.log(livro.autor) 
+
