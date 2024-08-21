754d84a261bc90d9096944a1313b76a125d97efb
Dudu malaquias <129951560+Zedapidamonhangaba@users.noreply.github.com>
2024-08-21 09:00:45 -0300

    Create turma.js

---
+++ turma.js
@@ -0,0 +1,17 @@
+const turma = {
+    nome: "3°A",
+    anoletivo: 2024,
+    professor: {
+        nome: "Gleiso",
+        disciplina: "sedução"
+    },
+    aluno:[{
+        nome:"Eduardo",
+        idade: 16
+    },
+    {
+        nome: "Claudio",
+        idade: 17
+    }
+]
+}
